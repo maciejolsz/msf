5592747aa8f34f1ddd0af096fee4c8eb8d3bc299
maciejolsz <maciejolsz@gmail.com>
2022-06-22 12:52:11 +0200

    -

--- db/migrate/20220618113307_create_members.rb
+++ db/migrate/20220618113307_create_members.rb
@@ -2,7 +2,7 @@ class CreateMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :members do |t|
       t.string :name
-      t.number :tcp
+      t.string :tcp
 
       t.timestamps
     end
